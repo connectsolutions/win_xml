d08d95ae295755f3752d1f9d4d807ed536c7d716
Richard Levenberg <richard.levenberg@cosocloud.com>
2017-07-04 09:00:42 -0700

    added working documentation

--- win_xml.py
+++ win_xml.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2015, Matt Davis <mdavis_ansible@rolpdog.com>
+# (c) 2017, Richard Levenberg <richard.levenberg@cosocloud.com>
 #
 # This file is part of Ansible
 #
@@ -21,11 +21,11 @@
 # this is a windows documentation stub.  actual code lives in the .ps1
 # file of the same name
 
-ANSIBLE_METADATA = {'status': ['preview'],
-                    'supported_by': 'core',
-                    'version': '1.0'}
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'core'}
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: win_xml
 version_added: "2.0"
@@ -35,27 +35,53 @@ description:
 options:
     path:
         description:
-        - The path of remote server's XML
+        - The path of remote servers XML
         required: true
-    xml:
+        aliases: [ dest, file ]
+    fragment:
         description:
         - The string representation of the XML fragment to be added
         required: true
+        aliases: [ xmlstring ]
     root:
         description:
         - The root of the remote server XML where the fragment will go
         required: false
         default: DocumentElement
+        aliases: [ xpath ]
     backup:
         description:
         - Whether to backup the remote server's XML before applying the change (yes/no)
         required: false
         default: no
-author: "Richard Levenberg (richard.levenberg@cosocloud.com)
+    type:
+        description:
+        - The type of XML you are working with
+        required: yes
+        default: element
+        choices:
+        - element
+        - attribute
+        - text
+    attribute:
+        description:
+        - "The attribute name IFF type is 'attribute'"
+        required: no
 
-EXAMPLES = '''
+author: "Richard Levenberg (richard.levenberg@cosocloud.com)"
+'''
+
+EXAMPLES = r'''
 # Apply our filter to Tomcat web.xml
-win_xml:
-    path: C:\\apache-tomcat\webapps\myapp\WEB-INF\web.xml
-    xml: "<filter><filter-name>MyFilter</filter-name><filter-class>com.example.MyFilter</filter-class></filter>"
+- win_xml:
+   path: C:\\apache-tomcat\webapps\myapp\WEB-INF\web.xml
+   fragment: '<filter><filter-name>MyFilter</filter-name><filter-class>com.example.MyFilter</filter-class></filter>'
+
+# Apply sslEnabledProtocols to Tomcat's server.xml
+- win_xml:
+   path: C:\\Tomcat\conf\server.xml
+   root: '//Server/Service[@name="Catalina"]/Connector[@port="9443"]'
+   attribute: 'sslEnabledProtocols'
+   fragment: 'TLSv1,TLSv1.1,TLSv1.2'
+   type: attribute
 '''
